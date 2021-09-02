5069190967e0df69daa3dcebcb3f2e98dcfcf98e
DanielFNG <daniel.gordon@ed.ac.uk>
2021-09-02 10:10:50 +0100

    OpenSimMoco progress

---
+++ Scripts/MocoSitToStand/example2DSitToStand_Predict.m
@@ -0,0 +1,115 @@
+%------------------------------------------------------------------------
+% Set up a gait prediction problem where the goal is to minimize effort
+% (squared controls) divided by distance traveled while enforcing symmetry of
+% the walking cycle and a prescribed average gait speed through endpoint
+% constraints. The solution of the coordinate tracking problem is
+% used as an initial guess for the prediction.
+
+% Load the Moco libraries
+import org.opensim.modeling.*;
+
+% Define the optimal control problem
+% ==================================
+study = MocoStudy();
+study.setName('sitToStandPrediction');
+
+problem = study.updProblem();
+modelProcessor = ModelProcessor(input_model);
+problem.setModelProcessor(modelProcessor);
+
+% Goals
+% =====
+
+model = modelProcessor.process();
+model.initSystem();
+
+% Effort over distance
+effortGoal = MocoControlGoal('effort', 1);
+%problem.addGoal(effortGoal); % Temporarily removing effort goal
+effortGoal.setExponent(2);
+
+% Fixed foot placement
+fixed_states = 'referenceFixedSittingStates.sto';
+footPlacementGoal = MocoTranslationTrackingGoal('no_slip', 1);
+tableProcessor = TableProcessor(fixed_states);
+footPlacementGoal.setStatesReference(tableProcessor);
+frames = org.opensim.modeling.StdVectorString();
+frames.add('/bodyset/calcn_r');
+frames.add('/bodyset/calcn_l');
+footPlacementGoal.setFramePaths(frames);
+problem.addGoal(footPlacementGoal);
+
+% Bounds
+% ======
+problem.setTimeBounds(0, [1.0, 2.0]);
+problem.setStateInfo('/jointset/groundPelvis/pelvis_tilt/value', [0*pi/180, 50*pi/180], 0.757922497, 0);
+problem.setStateInfo('/jointset/groundPelvis/pelvis_tx/value', [0, 0.5], 0.05028483);
+problem.setStateInfo('/jointset/groundPelvis/pelvis_ty/value', [0.5, 1.0], 0.54743397);
+problem.setStateInfo('/jointset/hip_l/hip_flexion_l/value', [-15*pi/180, 80*pi/180], 0.844694386, 0);
+problem.setStateInfo('/jointset/hip_r/hip_flexion_r/value', [-15*pi/180, 80*pi/180], 0.852378254, 0);
+problem.setStateInfo('/jointset/knee_l/knee_angle_l/value', [-120*pi/180, 5], -1.95480329, 0);
+problem.setStateInfo('/jointset/knee_r/knee_angle_r/value', [-120*pi/180, 5], -1.956778099, 0);
+problem.setStateInfo('/jointset/ankle_l/ankle_angle_l/value', [0*pi/180, 35*pi/180], 0.324237382, 0);
+problem.setStateInfo('/jointset/ankle_r/ankle_angle_r/value', [0*pi/180, 35*pi/180], 0.368424034, 0);
+problem.setStateInfo('/jointset/lumbar/lumbar/value', [-70, 0*pi/180], -0.928212182, 0);
+problem.setStateInfo('/jointset/groundPelvis/pelvis_tilt/speed', [], 0, 0);
+problem.setStateInfo('/jointset/groundPelvis/pelvis_tx/speed', [], 0, 0);
+problem.setStateInfo('/jointset/groundPelvis/pelvis_ty/speed', [], 0, 0);
+problem.setStateInfo('/jointset/hip_l/hip_flexion_l/speed', [], 0, 0);
+problem.setStateInfo('/jointset/hip_r/hip_flexion_r/speed', [], 0, 0);
+problem.setStateInfo('/jointset/knee_l/knee_angle_l/speed', [], 0, 0);
+problem.setStateInfo('/jointset/knee_r/knee_angle_r/speed', [], 0, 0);
+problem.setStateInfo('/jointset/ankle_l/ankle_angle_l/speed', [], 0, 0);
+problem.setStateInfo('/jointset/ankle_r/ankle_angle_r/speed', [], 0, 0);
+problem.setStateInfo('/jointset/lumbar/lumbar/speed', [], 0, 0);
+
+
+% Configure the solver
+% ====================
+solver = study.initCasADiSolver();
+solver.set_num_mesh_intervals(50);
+solver.set_verbosity(2);
+solver.set_optim_solver('ipopt');
+solver.set_optim_convergence_tolerance(1e-4);
+solver.set_optim_constraint_tolerance(1e-4);
+solver.set_optim_max_iterations(1000);
+solver.setGuess(sitToStandTrackingSolution); % Use tracking solution as initial guess
+
+
+% Solve problem
+% =============
+sitToStandPredictionSolution = study.solve();
+
+% Create a full stride from the periodic single step solution.
+% For details, view the Doxygen documentation for createPeriodicTrajectory().
+fullStride = opensimMoco.createPeriodicTrajectory(sitToStandPredictionSolution);
+fullStride.write('sitToStandPrediction_solution.sto');
+
+% Uncomment next line to visualize the result
+% study.visualize(fullStride);
+
+
+% Extract ground reaction forces
+% ==============================
+contact_r = StdVectorString();
+contact_l = StdVectorString();
+contact_r.add('contactHeel_r');
+contact_r.add('contactFront_r');
+contact_l.add('contactHeel_l');
+contact_l.add('contactFront_l');
+butt_r = StdVectorString();
+butt_l = StdVectorString();
+butt_r.add('chair_r');
+butt_l.add('chair_l');
+
+% Create a conventional ground reaction forces file by summing the contact
+% forces of contact spheres on each foot.
+% For details, view the Doxygen documentation for
+% createExternalLoadsTableForGait().
+externalForcesTableFlat = opensimMoco.createExternalLoadsTableForGait(model, ...
+    fullStride, contact_r, contact_l);
+STOFileAdapter.write(externalForcesTableFlat, ...
+    'sitToStandPrediction_solutionGRF.sto');
+chair_forces = opensimMoco.createExternalLoadsTableForGait(...
+    model, sitToStandTrackingSolution, butt_r, butt_l);
+STOFileAdapter.write(chair_forces, 'sitToStandPrediction_solutionChair.sto');
--- Scripts/MocoSitToStand/example2DSitToStand_Track.m
+++ Scripts/MocoSitToStand/example2DSitToStand_Track.m
@@ -72,12 +72,12 @@ track.setName('sitToStandTracking');
 % Note: If GRFTrackingWeight is set to 0 then GRFs will not be tracked. Setting
 % GRFTrackingWeight to 1 will cause the total tracking error (states + GRF) to
 % have about the same magnitude as control effort in the final objective value.
-controlEffortWeight = 0%10;
+controlEffortWeight = 0.00001;
 stateTrackingWeight = 1;
 
 % Reference data for tracking problem
 input_model = '2D_gait_scaled_contact.osim';
-input_data = 'referenceSitToStandCoordinates_reduced.sto';
+input_data = 'referenceSitToStandCoordinates.sto';
 tableProcessor = TableProcessor(input_data);
 tableProcessor.append(TabOpLowPassFilter(6));
 
@@ -120,17 +120,17 @@ problem.setStateInfo('/jointset/ankle_l/ankle_angle_l/value', [0*pi/180, 35*pi/1
 problem.setStateInfo('/jointset/ankle_r/ankle_angle_r/value', [0*pi/180, 35*pi/180], 0.368424034);
 problem.setStateInfo('/jointset/lumbar/lumbar/value', [-70, 0*pi/180], -0.928212182);
 
-% Speeds
-problem.setStateInfo('/jointset/groundPelvis/pelvis_tilt/speed', [0*pi/180, 50*pi/180], 0.757922497);
-problem.setStateInfo('/jointset/groundPelvis/pelvis_tx/v', [0, 0.5], 0.05028483);
-problem.setStateInfo('/jointset/groundPelvis/pelvis_ty/value', [0.5, 1.0], 0.54743397);
-problem.setStateInfo('/jointset/hip_l/hip_flexion_l/value', [-15*pi/180, 80*pi/180], 0.844694386);
-problem.setStateInfo('/jointset/hip_r/hip_flexion_r/value', [-15*pi/180, 80*pi/180], 0.852378254);
-problem.setStateInfo('/jointset/knee_l/knee_angle_l/value', [-120*pi/180, 5], -1.95480329);
-problem.setStateInfo('/jointset/knee_r/knee_angle_r/value', [-120*pi/180, 5], -1.956778099);
-problem.setStateInfo('/jointset/ankle_l/ankle_angle_l/value', [0*pi/180, 35*pi/180], 0.324237382);
-problem.setStateInfo('/jointset/ankle_r/ankle_angle_r/value', [0*pi/180, 35*pi/180], 0.368424034);
-problem.setStateInfo('/jointset/lumbar/lumbar/value', [-70, 0*pi/180], -0.928212182);
+% Speeds - inital and final to 0
+problem.setStateInfo('/jointset/groundPelvis/pelvis_tilt/speed', [-500, 500], 0, 0);
+problem.setStateInfo('/jointset/groundPelvis/pelvis_tx/speed', [-500, 500], 0, 0);
+problem.setStateInfo('/jointset/groundPelvis/pelvis_ty/speed', [-500, 500], 0, 0);
+problem.setStateInfo('/jointset/hip_l/hip_flexion_l/speed', [-500, 500], 0, 0);
+problem.setStateInfo('/jointset/hip_r/hip_flexion_r/speed', [-500, 500], 0, 0);
+problem.setStateInfo('/jointset/knee_l/knee_angle_l/speed', [-500, 500], 0, 0);
+problem.setStateInfo('/jointset/knee_r/knee_angle_r/speed', [-500, 500], 0, 0);
+problem.setStateInfo('/jointset/ankle_l/ankle_angle_l/speed', [-500, 500], 0, 0);
+problem.setStateInfo('/jointset/ankle_r/ankle_angle_r/speed', [-500, 500], 0, 0);
+problem.setStateInfo('/jointset/lumbar/lumbar/speed', [-500, 500], 0, 0);
 
 % Solve the problem
 % =================
@@ -158,149 +158,6 @@ STOFileAdapter.write(external_forces, 'sitToStandTracking_solutionGRF.sto');
 STOFileAdapter.write(chair_forces, 'sitToStandTracking_solutionChair.sto');
 
 
-% Uncomment next line to terminate after solving only the tracking problem
-return;
-
-%------------------------------------------------------------------------
-% Set up a gait prediction problem where the goal is to minimize effort
-% (squared controls) divided by distance traveled while enforcing symmetry of
-% the walking cycle and a prescribed average gait speed through endpoint
-% constraints. The solution of the coordinate tracking problem is
-% used as an initial guess for the prediction.
-
-
-% Define the optimal control problem
-% ==================================
-study = MocoStudy();
-study.setName('gaitPrediction');
-
-problem = study.updProblem();
-modelProcessor = ModelProcessor(input_model);
-problem.setModelProcessor(modelProcessor);
-
-
-% Goals
-% =====
-
-% Symmetry (to permit simulating only one step)
-symmetryGoal = MocoPeriodicityGoal('symmetryGoal');
-problem.addGoal(symmetryGoal);
-model = modelProcessor.process();
-model.initSystem();
-
-% Symmetric coordinate values (except for pelvis_tx) and speeds
-for i = 1:model.getNumStateVariables()
-    currentStateName = string(model.getStateVariableNames().getitem(i-1));
-    if startsWith(currentStateName , '/jointset')
-        if contains(currentStateName,'_r')
-            pair = MocoPeriodicityGoalPair(currentStateName, ...
-                           regexprep(currentStateName,'_r','_l') );
-            symmetryGoal.addStatePair(pair);
-        end
-        if contains(currentStateName,'_l')
-            pair = MocoPeriodicityGoalPair(currentStateName, ...
-                           regexprep(currentStateName,'_l','_r'));
-            symmetryGoal.addStatePair(pair);
-        end
-        if (~contains(currentStateName,'_r') && ...
-            ~contains(currentStateName,'_l') && ...
-            ~contains(currentStateName,'pelvis_tx/value')  && ...
-            ~contains(currentStateName,'/activation'))
-            symmetryGoal.addStatePair(MocoPeriodicityGoalPair(currentStateName));
-        end
-    end
-end
-
-% Symmetric muscle activations
-for i = 1:model.getNumStateVariables()
-    currentStateName = string(model.getStateVariableNames().getitem(i-1));
-    if endsWith(currentStateName,'/activation')
-        if contains(currentStateName,'_r')
-            pair = MocoPeriodicityGoalPair(currentStateName, ...
-                           regexprep(currentStateName,'_r','_l'));
-            symmetryGoal.addStatePair(pair);
-        end
-        if contains(currentStateName,'_l')
-            pair = MocoPeriodicityGoalPair(currentStateName, ...
-                           regexprep(currentStateName,'_l','_r'));
-            symmetryGoal.addStatePair(pair);
-        end
-    end
-end
-
-% Symmetric coordinate actuator controls
-symmetryGoal.addControlPair(MocoPeriodicityGoalPair('/lumbarAct'));
-
-
-% Prescribed average gait speed
-speedGoal = MocoAverageSpeedGoal('speed');
-problem.addGoal(speedGoal);
-speedGoal.set_desired_average_speed(1.2);
-
-% Effort over distance
-effortGoal = MocoControlGoal('effort', 10);
-problem.addGoal(effortGoal);
-effortGoal.setExponent(3);
-effortGoal.setDivideByDisplacement(true);
-
-
-% Bounds
-% ======
-problem.setTimeBounds(0, [0.4, 0.6]);
-problem.setStateInfo('/jointset/groundPelvis/pelvis_tilt/value', [-20*pi/180, -10*pi/180]);
-problem.setStateInfo('/jointset/groundPelvis/pelvis_tx/value', [0, 1]);
-problem.setStateInfo('/jointset/groundPelvis/pelvis_ty/value', [0.75, 1.25]);
-problem.setStateInfo('/jointset/hip_l/hip_flexion_l/value', [-10*pi/180, 60*pi/180]);
-problem.setStateInfo('/jointset/hip_r/hip_flexion_r/value', [-10*pi/180, 60*pi/180]);
-problem.setStateInfo('/jointset/knee_l/knee_angle_l/value', [-50*pi/180, 0]);
-problem.setStateInfo('/jointset/knee_r/knee_angle_r/value', [-50*pi/180, 0]);
-problem.setStateInfo('/jointset/ankle_l/ankle_angle_l/value', [-15*pi/180, 25*pi/180]);
-problem.setStateInfo('/jointset/ankle_r/ankle_angle_r/value', [-15*pi/180, 25*pi/180]);
-problem.setStateInfo('/jointset/lumbar/lumbar/value', [0, 20*pi/180]);
-
-
-% Configure the solver
-% ====================
-solver = study.initCasADiSolver();
-solver.set_num_mesh_intervals(50);
-solver.set_verbosity(2);
-solver.set_optim_solver('ipopt');
-solver.set_optim_convergence_tolerance(1e-4);
-solver.set_optim_constraint_tolerance(1e-4);
-solver.set_optim_max_iterations(1000);
-solver.setGuess(sitToStandTrackingSolution); % Use tracking solution as initial guess
-
-
-% Solve problem
-% =============
-gaitPredictionSolution = study.solve();
-
-% Create a full stride from the periodic single step solution.
-% For details, view the Doxygen documentation for createPeriodicTrajectory().
-fullStride = opensimMoco.createPeriodicTrajectory(gaitPredictionSolution);
-fullStride.write('sitToStandPrediction_solution.sto');
-
-% Uncomment next line to visualize the result
-% study.visualize(fullStride);
-
-
-% Extract ground reaction forces
-% ==============================
-contact_r = StdVectorString();
-contact_l = StdVectorString();
-contact_r.add('contactHeel_r');
-contact_r.add('contactFront_r');
-contact_l.add('contactHeel_l');
-contact_l.add('contactFront_l');
-
-% Create a conventional ground reaction forces file by summing the contact
-% forces of contact spheres on each foot.
-% For details, view the Doxygen documentation for
-% createExternalLoadsTableForGait().
-externalForcesTableFlat = opensimMoco.createExternalLoadsTableForGait(model, ...
-                             fullStride, contact_r, contact_l);
-STOFileAdapter.write(externalForcesTableFlat, ...
-                             'sitToStandPrediction_solutionGRF.sto');
 
 
 
