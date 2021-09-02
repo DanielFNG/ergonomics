164d0875b23c47be8c71a6defe6e6ff04ef9edc8
Daniel Gordon <daniel.gordon@ed.ac.uk>
2021-09-02 17:09:17 +0100

    Adjust initial pose & add foot velocity goal

--- Scripts/MocoSitToStand/example2DSitToStand_Predict.m
+++ Scripts/MocoSitToStand/example2DSitToStand_Predict.m
@@ -26,7 +26,7 @@ model.initSystem();
 % Effort over distance
 effortGoal = MocoControlGoal('effort', 1);
 %problem.addGoal(effortGoal); % Temporarily removing effort goal
-effortGoal.setExponent(2);
+effortGoal.setExponent(3);
 
 % Fixed foot placement
 fixed_states = 'referenceFixedSittingStates_dropped.sto';
@@ -37,28 +37,36 @@ frames = org.opensim.modeling.StdVectorString();
 frames.add('/bodyset/calcn_r');
 frames.add('/bodyset/calcn_l');
 footPlacementGoal.setFramePaths(frames);
-problem.addGoal(footPlacementGoal);
+%problem.addGoal(footPlacementGoal);
 
 % Fixed foot orientation
 footOrientationGoal = MocoOrientationTrackingGoal('no_rotate', 1);
 footOrientationGoal.setStatesReference(tableProcessor);
 footOrientationGoal.setFramePaths(frames);
-problem.addGoal(footOrientationGoal);
+%problem.addGoal(footOrientationGoal);
+
+% Minimise foot velocity
+r_foot_v = MocoOutputGoal('rfv', 1);
+r_foot_v.setOutputPath('/bodyset/calcn_r|linear_velocity');
+r_foot_w = MocoOutputGoal('rfw', 1);
+r_foot_w.setOutputPath('/bodyset/calcn_r|angular_velocity');
+problem.addGoal(r_foot_v);
+problem.addGoal(r_foot_w);
 
 
 % Bounds
 % ======
 problem.setTimeBounds(0, [1.0, 2.0]);
-problem.setStateInfo('/jointset/groundPelvis/pelvis_tilt/value', [0*pi/180, 50*pi/180], 0.757922497, 0);
-problem.setStateInfo('/jointset/groundPelvis/pelvis_tx/value', [0, 0.5], 0.05028483);
-problem.setStateInfo('/jointset/groundPelvis/pelvis_ty/value', [0.5, 1.0], 0.54743397);
-problem.setStateInfo('/jointset/hip_l/hip_flexion_l/value', [-15*pi/180, 80*pi/180], 0.844694386, 0);
-problem.setStateInfo('/jointset/hip_r/hip_flexion_r/value', [-15*pi/180, 80*pi/180], 0.852378254, 0);
-problem.setStateInfo('/jointset/knee_l/knee_angle_l/value', [-120*pi/180, 5], -1.95480329, 0);
-problem.setStateInfo('/jointset/knee_r/knee_angle_r/value', [-120*pi/180, 5], -1.956778099, 0);
-problem.setStateInfo('/jointset/ankle_l/ankle_angle_l/value', [0*pi/180, 35*pi/180], 0.324237382, 0);
-problem.setStateInfo('/jointset/ankle_r/ankle_angle_r/value', [0*pi/180, 35*pi/180], 0.368424034, 0);
-problem.setStateInfo('/jointset/lumbar/lumbar/value', [-70*pi/180, 0*pi/180], -0.928212182, 0);
+problem.setStateInfo('/jointset/groundPelvis/pelvis_tilt/value', [0*pi/180, 50*pi/180], 43.426*pi/180, 0);
+problem.setStateInfo('/jointset/groundPelvis/pelvis_tx/value', [0, 0.5], 0.05);
+problem.setStateInfo('/jointset/groundPelvis/pelvis_ty/value', [0.5, 1.0], 0.536);
+problem.setStateInfo('/jointset/hip_l/hip_flexion_l/value', [-15*pi/180, 80*pi/180], 47.148*pi/180, 0);
+problem.setStateInfo('/jointset/hip_r/hip_flexion_r/value', [-15*pi/180, 80*pi/180], 47.148*pi/180, 0);
+problem.setStateInfo('/jointset/knee_l/knee_angle_l/value', [-120*pi/180, 5], -109.565*pi/180, 0);
+problem.setStateInfo('/jointset/knee_r/knee_angle_r/value', [-120*pi/180, 5], -109.565*pi/180, 0);
+problem.setStateInfo('/jointset/ankle_l/ankle_angle_l/value', [0*pi/180, 35*pi/180], 21.109*pi/180, 0);
+problem.setStateInfo('/jointset/ankle_r/ankle_angle_r/value', [0*pi/180, 35*pi/180], 21.109*pi/180, 0);
+problem.setStateInfo('/jointset/lumbar/lumbar/value', [-70*pi/180, 0*pi/180], -53.183*pi/180, 0);
 problem.setStateInfo('/jointset/groundPelvis/pelvis_tilt/speed', [-500, 500], 0, 0);
 problem.setStateInfo('/jointset/groundPelvis/pelvis_tx/speed', [-500, 500], 0, 0);
 problem.setStateInfo('/jointset/groundPelvis/pelvis_ty/speed', [-500, 500], 0, 0);
