c0eeb5c3b26a4be595b59949f527b0b90663878a
Daniel Gordon <daniel.gordon@ed.ac.uk>
2021-10-15 09:01:36 +0100

    Fix implementation of margin of stability

--- Source/cpp/MocoMarginOfStabilityGoal.cpp
+++ Source/cpp/MocoMarginOfStabilityGoal.cpp
@@ -20,14 +20,26 @@ void MocoMarginOfStabilityGoal::calcIntegrandImpl(
     // Update model positions
     getModel().realizeDynamics(input.state);
 
+    /*
+
+    const auto& force = getModel().getComponent
+        <SmoothSphereHalfSpaceForce>("contactHeel_l");
+    Array<double> force_values = force.getRecordValues(input.state);
+
+    const auto& geometries = getModel().getContactGeometrySet();
+    const auto& ground_transform = 
+        geometries.get("heel_l").getFrame().getTransformInGround(input.state);
+
+    SimTK::Vec3 ground_point = 
+        ground_transform * geometries.get("heel_l").get_location();
+
+    integrand = ground_point[1];
+
+    */
+
     // Access required contact forces.
     std::vector<std::string> force_strings = {
-        "contactHeel_l", 
-        "contactHeel_r", 
-        "contactFront_l", 
-        "contactFront_r", 
-        "chair_l", 
-        "chair_r"
+        "contactHeel_l", "contactHeel_r", "contactFront_l", "contactFront_r", "chair_l", "chair_r"
     };
 
     // Access required contact geometries
@@ -36,7 +48,9 @@ void MocoMarginOfStabilityGoal::calcIntegrandImpl(
     };
         
     // Create BoS polygon
+    
     polygon_2d poly;
+    bool empty = true;
     for (int i = 0; i < force_strings.size(); i++) {
         // Compute force at contact point
         const auto& force = getModel().getComponent
@@ -46,54 +60,62 @@ void MocoMarginOfStabilityGoal::calcIntegrandImpl(
         // If we register a vertical force, this point is active, so we append 
         // it to our BoS polygon
         if (force_values[1] > force.get_constant_contact_force()) {
-            // Get contact sphere
+            // Get contact spheres
             const auto& geometries = getModel().getContactGeometrySet();
-            ContactSphere sphere = dynamic_cast<const ContactSphere&>(
-                geometries.get(sphere_strings[i]));
 
             // Compute transformation from sphere frame to ground frame
             const auto& ground_transform = 
-                sphere.getFrame().getTransformInGround(input.state);
+                geometries.get(sphere_strings[i]).getFrame().getTransformInGround(input.state);
 
             // Transform sphere location to ground frame
             SimTK::Vec3 ground_point = 
-                ground_transform * sphere.getLocation();
+                ground_transform * geometries.get(sphere_strings[i]).get_location();
 
             // Append the projected 2D point to our polygon
             append(poly.outer(), make<point_2d>(
                 ground_point.get(0), ground_point.get(2)));
+
+            // Note that we have at least one point in our polygon
+            empty = false;
         }
     }
 
     // Close the polygon & make sure it is directed clockwise
     correct(poly);
 
-    // Compute the centre of the polygon
-    point_2d cent;
-    centroid(poly, cent);
+    // If our polygon is non-empty...
+    if (!empty) {
 
-    // Compute the CoM of the model given the current state
-    double Mass = 0.0;
-    double com[3] = {0.0, 0.0, 0.0};
-    SimTK::Vec3 vec;
-    BodySet bs = getModel().getBodySet();
-    for (int i = 0; i < bs.getSize(); i++) {
-        Body body = bs.get(i);
-        const SimTK::Vec3 body_com = body.get_mass_center();
-        vec = body.findStationLocationInGround(input.state, body_com);
-        Mass += body.get_mass();
-        com[0] += body.get_mass() * vec[0];
-        com[1] += body.get_mass() * vec[1];
-        com[2] += body.get_mass() * vec[2];
+        // Compute the centre of the polygon
+        point_2d cent;
+        centroid(poly, cent);
+
+        // Compute the CoM of the model given the current state
+        double Mass = 0.0;
+        double com[3] = {0.0, 0.0, 0.0};
+        SimTK::Vec3 vec;
+        const auto& bs = getModel().getBodySet();
+        for (int i = 0; i < bs.getSize(); i++) {
+            const auto& body = bs.get(i);
+            const SimTK::Vec3 body_com = body.get_mass_center();
+            vec = body.findStationLocationInGround(input.state, body_com);
+            Mass += body.get_mass();
+            com[0] += body.get_mass() * vec[0];
+            com[1] += body.get_mass() * vec[1];
+            com[2] += body.get_mass() * vec[2];
+        }
+        com[0] /= Mass;
+        com[1] /= Mass;
+        com[2] /= Mass;
+
+        // Compute the distance between the polygon centre and the CoM projection
+        point_2d com_projection;
+        assign_values(com_projection, com[0], com[2]);
+        integrand = distance(com_projection, cent);
+
+    } else {
+        integrand = 10;
     }
-    com[0] /= Mass;
-    com[1] /= Mass;
-    com[2] /= Mass;
-
-    // Compute the distance between the polygon centre and the CoM projection
-    point_2d com_projection;
-    assign_values(com_projection, com[0], com[2]);
-    integrand = distance(com_projection, cent);
 }
 
 void MocoMarginOfStabilityGoal::calcGoalImpl(
