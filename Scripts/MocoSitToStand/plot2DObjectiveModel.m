e84c2fa0f1d381a8713e629312b2fb5155be8dfe
Daniel Gordon <daniel.gordon@ed.ac.uk>
2021-10-17 19:47:44 +0100

    Reorganisation

---
+++ Scripts/MocoSitToStand/plot2DObjectiveModel.m
@@ -0,0 +1,37 @@
+function plot2DObjectiveModel(results, n_points)
+% Visualise the results of a 2D Bayesopt run. 
+%
+% Input: results - a bayesopt results object
+%        n_points - # of points to feed to linspace in the x/y vector
+
+    % Check function dimensions
+    if length(results.VariableDescriptions) ~= 2
+        error('This function is designed exclusively for 2D bayesopt.');
+    end
+
+    % Get variable ranges
+    x_range = results.VariableDescriptions(1).Range;
+    y_range = results.VariableDescriptions(2).Range;
+    
+    % Create arrays of specified size
+    x = linspace(x_range(1), x_range(2), n_points);
+    y = linspace(y_range(1), y_range(2), n_points);
+    
+    % Get coordinates of prediction points
+    [xg, yg] = meshgrid(x, y);
+    coordinates = reshape(cat(2, xg', yg'), [], 2);
+    
+    % Predict function value on all coordinates
+    z = predict(results.ObjectiveFcnModel, coordinates);
+
+    % Produce resulting plot
+    figure;
+    %surf(x, y, transpose(reshape(z, [n_points, n_points])));
+    surf(x, y, reshape(z, [n_points, n_points]));
+    
+    % Overplot the sampled points
+    hold on;
+    scatter3(results.XTrace{:, 1}, results.XTrace{:, 2}, ...
+        results.ObjectiveTrace, 100, 'b', 'filled');
+
+end
