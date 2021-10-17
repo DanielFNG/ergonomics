2759e04abc509ee1b610544ff23ffe533ca53b79
Daniel Gordon <daniel.gordon@ed.ac.uk>
2021-10-17 20:00:38 +0100

    Rework UpperLevelData to work with local output

--- Scripts/UpperLevelData/main.m
+++ Scripts/UpperLevelData/main.m
@@ -1,33 +1,36 @@
 %% Define inputs
 model = 'gait2392_markers_scaled.osim';
 data_dir = [pwd filesep 'Data'];
-analyse_dir = [pwd filesep 'BK'];
-normalise_dir = [pwd filesep 'Normalised'];
-bk_settings = 'C:\Users\danie\Documents\GitHub\opensim-matlab\Defaults\BK\bk.xml';
+output_dir = createOutputFolder(pwd);
+bk_settings = 'bk.xml';
 n_points = 101;
 labels = {'center_of_mass_X', 'center_of_mass_Y', 'center_of_mass_Z'};
 
 %% Produce Normalised BK
 
-% Create directory
+% Directories
+bk_dir = [output_dir filesep 'BK'];
+normalise_dir = [output_dir filesep 'Normalised'];
 mkdir(normalise_dir);
 
 % Get marker data
 [n, files] = getFilePaths(data_dir, '.mot');
 
+% Specify 
+
 % For each IK...
 for i = 1:n
     
     % Run Body Kinematics
-    runAnalyse(num2str(i), model, files{i}, [], analyse_dir, bk_settings);
+    runAnalyse(num2str(i), model, files{i}, [], bk_dir, bk_settings);
     
     % Identify start/end times for the sit-to-stand
-    velocity = Data([analyse_dir filesep num2str(i) ...
+    velocity = Data([bk_dir filesep num2str(i) ...
         '_BodyKinematics_vel_global.sto']);
     [start, finish] = findSitToStandTimes(velocity);
     
     % Slice the BK position 
-    bk = Data([analyse_dir filesep num2str(i) '_BodyKinematics_pos_global.sto']);
+    bk = Data([bk_dir filesep num2str(i) '_BodyKinematics_pos_global.sto']);
     bk = bk.slice(start, finish);
     bk.writeToFile([normalise_dir filesep num2str(i) '_BK_Positions']);
     
