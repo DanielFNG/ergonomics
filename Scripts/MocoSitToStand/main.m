ebf7672586f1251df9ea8574d74cf5054ef01931
Daniel Gordon <daniel.gordon@ed.ac.uk>
2021-10-19 10:18:22 +0100

    Continued work on main script

--- Scripts/MocoSitToStand/main.m
+++ Scripts/MocoSitToStand/main.m
@@ -1,3 +1,11 @@
+%% Inputs
+osim = '';
+reference_states = '';
+
+
 ProduceReferenceCoordinates;
 
 DefineBounds;
+
+name, w_states, w_controls, osim, input)
+solution = produceTrackingGuess('TrackingGuess', );
