8f2371e6a49fa2ff29b23d27fa6685b0dbc93a52
Daniel Gordon <daniel.gordon@ed.ac.uk>
2021-10-19 10:17:31 +0100

    Adjust UpperLevelData script to produce mean CoM info file

--- Scripts/UpperLevelData/main.m
+++ Scripts/UpperLevelData/main.m
@@ -13,10 +13,8 @@ bk_dir = [output_dir filesep 'BK'];
 normalise_dir = [output_dir filesep 'Normalised'];
 mkdir(normalise_dir);
 
-% Get marker data
+% Get IK data
 [n, files] = getFilePaths(data_dir, '.mot');
-
-% Specify 
 
 % For each IK...
 for i = 1:n
@@ -45,10 +43,12 @@ end
 com = zeros(n_points, n, 3);
 means = zeros(n_points, 3);
 sdevs = zeros(n_points, 3);
+times = zeros(1, 3);
 
 % Get X/Y/Z CoM positions
 for i = 1:n
     positions = Data(files{i});
+    times(i) = positions.getTotalTime();
     for j = 1:3
         com(:, i, j) = stretchVector(positions.getColumn(labels{j}), n_points);
     end
@@ -56,9 +56,17 @@ end
 
 % Compute means & standard deviations 
 for i = 1:3
+    mean_time = mean(times);
     means(:, i) = mean(com(:, :, i), 2);
     sdevs(:, i) = std(com(:, :, i), 0, 2);
 end
 
 % Visualise results
-plotShadedSDevs({com(:, :, 1)', com(:, :, 2)'}, {'X', 'Y'}, '% of Movement', 'Position (m)', 0:100)
+plotShadedSDevs({com(:, :, 1)', com(:, :, 2)'}, {'X', 'Y'}, '% of Movement', 'Position (m)', 0:100);
+
+%% Produce a file saving the mean CoM trajectories (OpenSim format)
+labels = {'time', 'center_of_mass_X', 'center_of_mass_Y', ...
+    'center_of_mass_Z'};
+timesteps = linspace(0, mean_time, 101);
+mean_file = STOData([timesteps', means], {}, labels);
+mean_file.writeToFile([output_dir filesep 'means.sto']);
