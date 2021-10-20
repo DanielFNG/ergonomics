a6c3bd3e8d8dddcc26414a44ef4b9780e62d7ebb
DanielFNG <daniel.gordon@ed.ac.uk>
2021-10-20 07:52:08 +0100

    Update main.m

--- Scripts/MocoSitToStand/main.m
+++ Scripts/MocoSitToStand/main.m
@@ -2,10 +2,11 @@
 osim = '';
 reference_states = '';
 
-
-ProduceReferenceCoordinates;
-
+%% Define bounds for both the tracking & prediction problems
 DefineBounds;
+
+%% Produce reference coordinates for the tracking problem from input data
+ProduceReferenceCoordinates;
 
 name, w_states, w_controls, osim, input)
 solution = produceTrackingGuess('TrackingGuess', );
