cc2e57194da0c4307591cf208fac4c3fbe77bd86
Daniel Gordon <daniel.gordon@ed.ac.uk>
2021-10-21 00:31:43 +0100

    Bugfix

--- Scripts/Landscape/writeSolutionToFile.m
+++ Scripts/Landscape/writeSolutionToFile.m
@@ -16,19 +16,19 @@ function writeSolutionToFile(solution, save_dir, X)
     reduced_name = [save_dir filesep 'reduced' name '.sto'];
     
     % Unseal solution if needed, to be printed for posterity
-    if ~success
-        solution.unseal();
+    if ~solution.isSealed()
+    
+        % Write the full solution to file
+        solution.write(save_name);
+
+        % Note: was getting some sort of bug where the Gamma variables in
+        % the last timestep of the solution file were NaNs. These variables
+        % are actually not needed for the BK so we can filter them out
+        % manually with the following.
+        [values, labels, header] = MOTSTOTXTData.load(save_name);
+        reduced = STOData(values(:, 1:end-6), header, labels(1:end-6));
+        reduced.writeToFile(reduced_name);
+        
     end
-    
-    % Write the full solution to file
-    solution.write(save_name);
-    
-    % Note: was getting some sort of bug where the Gamma variables in
-    % the last timestep of the solution file were NaNs. These variables
-    % are actually not needed for the BK so we can filter them out
-    % manually with the following.
-    [values, labels, header] = MOTSTOTXTData.load(save_name);
-    reduced = STOData(values(:, 1:end-6), header, labels(1:end-6));
-    reduced.writeToFile(reduced_name);
 
 end
