c3aabef4a71186cd41bceb008f03c851d493ba43
DanielFNG <daniel.gordon@ed.ac.uk>
2021-11-29 13:44:55 +0000

    sending code to michael

---
+++ Source/cpp/cpp_code/MocoMarginOfStabilityGoal.h
@@ -0,0 +1,35 @@
+#ifndef OPENSIM_MOCOMARGINOFSTABILITYGOAL_H
+#define OPENSIM_MOCOMARGINOFSTABILITYGOAL_H
+
+#include <OpenSim/Moco/osimMoco.h>
+#include "osimMocoMarginOfStabilityGoalDLL.h"
+
+namespace OpenSim {
+
+class OSIMMOCOMARGINOFSTABILITYGOAL_API MocoMarginOfStabilityGoal : public MocoGoal {
+    OpenSim_DECLARE_CONCRETE_OBJECT(MocoMarginOfStabilityGoal, MocoGoal);
+
+public:
+    MocoMarginOfStabilityGoal() { }
+    MocoMarginOfStabilityGoal(std::string name) : MocoGoal(std::move(name)) { }
+    MocoMarginOfStabilityGoal(std::string name, double weight)
+            : MocoGoal(std::move(name), weight) { }
+
+protected:
+    Mode getDefaultModeImpl() const override { return Mode::Cost; }
+    bool getSupportsEndpointConstraintImpl() const override { return true; }
+    void initializeOnModelImpl(const Model&) const override;
+    void calcIntegrandImpl(
+            const IntegrandInput& input, double& integrand) const override;
+    void calcGoalImpl(
+            const GoalInput& input, SimTK::Vector& cost) const override;
+
+private:
+    // Properties
+    std::vector<SmoothSphereHalfSpaceForce*> contact_pointers;
+
+};
+
+} // namespace OpenSim
+
+#endif // OPENSIM_MOCOCUSTOMEFFORTGOAL_H
