24434de7770282cdd4c3c5db7b19fce382744acd
Daniel Gordon <daniel.gordon@ed.ac.uk>
2022-03-24 14:17:48 +0000

    Finish new cluster implementation

--- Scripts/12_Cluster/main.m
+++ Scripts/12_Cluster/main.m
@@ -3,40 +3,73 @@ upper_limit = 0.1;
 lower_limit = 0;
 reference_weights = [0.02, 0.04, 0.02, 0.08];
 executable = 'optimise4D_cluster'; % Same as optimise 4D but produces GRF file
-results_dir = '/exports/eddie/scratch/dgordon3/results';
+cluster_script = 'cluster.sh';
+script_dir = pwd;
+output_dir = '/exports/eddie/scratch/dgordon3/results_testrun';
+population_size = 10; % THIS MUST BE IN SYNC WITH .SH JOB FILE!
+max_generations = 2;
 
 % Inputs
-output_dir = createOutputFolder(results_dir);
 reference_path = [output_dir filesep 'reference.sto'];
-upper_objective = @compareRelativeKinematicsAndGRFs;
 model_path = '2D_gait_jointspace_welded.osim';
 tracking_path = 'guess.sto';
 
+% Create output directory
+mkdir(output_dir);
+
 % Generate reference motion if needed
 if ~isfile(reference_path)
-    mocoExecutableInterface(executable, model_path, tracking_path, ...
-        reference_path, reference_weights, true, false);
+    command = ['~/ergonomics/bin/optimise4D_cluster ' model_path ' ' ...
+        guess_path ' ' reference_path ' none'];
+    for i = 1:length(weights)
+        command = [command ' ' num2str(weights(i))];
+    end
+    [~, ~] = system(command);
 end
-
-% Process reference data 
-reference = Data(reference_path);
-% labels = reference.Labels(1:end - 6);
-labels = {'/jointset/lumbar/lumbar/value', '/jointset/hip_r/hip_flexion_r/value', ...
-    '/jointset/knee_r/knee_angle_r/value', '/jointset/ankle_r/ankle_angle_r/value'};
-contacts = {'chair_r', 'r_1', 'r_2', 'r_3', 'r_4'};
-upper_args = {reference, labels, contacts, model_path};
-
-% Define objective 
-outer_objective = @ (weights) objective(...
-    upper_objective, upper_args, output_dir, weights);
 
 % Use GA to run optimisation
 n_parameters = length(reference_weights);
-options = optimoptions('ga', 'Display', 'iter', ...
-    'MaxGenerations', 3, 'PlotFcn', 'gaplotscores', ...
-    'PopulationSize', 10, 'UseVectorized', true); 
+options = optimoptions('ga', 'Display', 'iter', 'UseVectorized', true, ...
+    'MaxGenerations', max_generations, 'PopulationSize', population_size); 
 lb = lower_limit*ones(1, n_parameters);
 ub = upper_limit*ones(1, n_parameters);
 [x, fval, exitflag, output, population, scores] = ga(...
-    outer_objective, n_parameters, [], [], [], [], lb, ub, [], options);
+    @upper_objective, n_parameters, [], [], [], [], lb, ub, [], options);
+
+% Save result
+save([output_dir filesep 'results.mat']);
+
+function results = upper_objective(weights)
+
+    % Create an inner results folder based on time
+    folder = [output_dir filesep datestr(datetime('now'), 'yy-mm-dd_hh-MM-ss')];
+    mkdir(folder);
+
+    % Write weights.txt file
+    weights_file = [script_dir filesep 'weights.txt'];
+    fid = fopen(weights_file, 'w');
+    fprintf(fid, [repmat('%f ', 1, n_parameters) '\n'], weights);
+    fclose(fid);
+
+    % Execute cluster run
+    system(['qsub -sync y ' cluster_script]);
+
+    % Read results files
+    results = zeros(population_size, 1);
+    for i = 1:population_size
+        try
+            filename = [num2str(i) '.txt.'];
+            fid = fopen(filename);
+            results(i) = fscanf(fid, '%f');
+            fclose(fid);
+            movefile(filename, folder);
+        catch
+            results(i) = nan;
+        end
+    end
+
+    % Move weights file from workspace dir to output dir
+    movefile(weights_file, folder);
+
+end
 
