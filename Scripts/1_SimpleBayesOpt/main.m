6b9e471a10cfa0bfdad29935e90edaee70468deb
Daniel Gordon <danielfng@live.co.uk>
2022-07-01 18:32:27 +0100

    working code & results

--- Scripts/1_SimpleBayesOpt/main.m
+++ Scripts/1_SimpleBayesOpt/main.m
@@ -16,9 +16,11 @@ end
 
 %% Bayesian Optimisation
 
-tic;
+% tic;
 
 for i = 1:n_functions
+
+    tic;
 
     % Set up optimisable variables
     optimisation_variables = [];
@@ -36,63 +38,68 @@ for i = 1:n_functions
         bayesopt(objective, optimisation_variables, 'MaxObjectiveEvaluations', max_evaluations); 
     noisy.bayesopt(i) = ...
         bayesopt(noisy_objective, optimisation_variables, 'MaxObjectiveEvaluations', max_evaluations);
+
+    partial_time(i) = toc;
+
+    save(['baysopt' num2str(i) '.mat'], 'deterministic', 'noisy', 'partial_time');
+    
 end
 
 time.bayesopt = toc;
 
 
-%% Surrogate Optimisation
-
-tic;
-
-for i = 1:n_functions
-
-    % Set up some options including initial sample points. Otherwise, the
-    % algorithm generates these points itself, and includes the midpoint of
-    % the domain - typically (0,0) which is typically the true minimum!
-    initial_points = (rand(max(20, 2*dimension(i)), dimension(i)) - 0.5)*2*ub(i);
-    options = optimoptions('surrogateopt', 'Display', 'iter', ...
-        'MaxFunctionEvaluations', max_evaluations, 'InitialPoints', initial_points);
-
-    % Run
-    [x, fval] = surrogateopt(handles{i}, ...
-        ones(1, dimension(i))*lb(i), ones(1, dimension(i))*ub(i), [], [], [], [], [], options);
-    deterministic.surrogate(i) = struct('x', x, 'fval', fval);
-    [x, fval] = surrogateopt(noisy_handles{i}, ...
-        ones(1, dimension(i))*lb(i), ones(1, dimension(i))*ub(i), [], [], [], [], [], options);
-    noisy.surrogate(i) = struct('x', x, 'fval', fval);
-
-end
-
-time.surrogate = toc;
-
-%% Genetic Optimisation
-
-tic;
-
-for i = 1:n_functions
-
-    if dimension(i) <= 5
-        pop_size = 50;
-    else
-        pop_size = 200;
-    end
-    max_gen = floor(max_evaluations/pop_size);
-    options = optimoptions('ga', ...
-        'Display', 'iter', 'PlotFcn', 'gaplotscores', 'MaxGenerations', max_gen);
-
-    [x, fval] = ga(handles{i}, dimension(i), [], [], [], [], ...
-        ones(1, dimension(i))*lb(i), ones(1, dimension(i))*ub(i), [] , options);
-    deterministic.ga(i) = struct('x', x, 'fval', fval);
-
-    [x, fval] = ga(noisy_handles{i}, dimension(i), [], [], [], [], ...
-        ones(1, dimension(i))*lb(i), ones(1, dimension(i))*ub(i), [] , options);
-    noisy.ga(i) = struct('x', x, 'fval', fval);
-
-
-end
-
-time.ga = toc;
+% %% Surrogate Optimisation
+% 
+% tic;
+% 
+% for i = 1:n_functions
+% 
+%     % Set up some options including initial sample points. Otherwise, the
+%     % algorithm generates these points itself, and includes the midpoint of
+%     % the domain - typically (0,0) which is typically the true minimum!
+%     initial_points = (rand(max(20, 2*dimension(i)), dimension(i)) - 0.5)*2*ub(i);
+%     options = optimoptions('surrogateopt', 'Display', 'iter', ...
+%         'MaxFunctionEvaluations', max_evaluations, 'InitialPoints', initial_points);
+% 
+%     % Run
+%     [x, fval] = surrogateopt(handles{i}, ...
+%         ones(1, dimension(i))*lb(i), ones(1, dimension(i))*ub(i), [], [], [], [], [], options);
+%     deterministic.surrogate(i) = struct('x', x, 'fval', fval);
+%     [x, fval] = surrogateopt(noisy_handles{i}, ...
+%         ones(1, dimension(i))*lb(i), ones(1, dimension(i))*ub(i), [], [], [], [], [], options);
+%     noisy.surrogate(i) = struct('x', x, 'fval', fval);
+% 
+% end
+% 
+% time.surrogate = toc;
+% 
+% %% Genetic Optimisation
+% 
+% tic;
+% 
+% for i = 1:n_functions
+% 
+%     if dimension(i) <= 5
+%         pop_size = 50;
+%     else
+%         pop_size = 200;
+%     end
+%     max_gen = floor(max_evaluations/pop_size);
+%     options = optimoptions('ga', ...
+%         'Display', 'iter', 'PlotFcn', 'gaplotscores', 'MaxGenerations', max_gen);
+% 
+%     [x, fval] = ga(handles{i}, dimension(i), [], [], [], [], ...
+%         ones(1, dimension(i))*lb(i), ones(1, dimension(i))*ub(i), [] , options);
+%     deterministic.ga(i) = struct('x', x, 'fval', fval);
+% 
+%     [x, fval] = ga(noisy_handles{i}, dimension(i), [], [], [], [], ...
+%         ones(1, dimension(i))*lb(i), ones(1, dimension(i))*ub(i), [] , options);
+%     noisy.ga(i) = struct('x', x, 'fval', fval);
+% 
+% 
+% end
+% 
+% time.ga = toc;
 
 %% Helper Functions 
 
