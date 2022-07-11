8384dee1c2d5119c00035059cf0cc9f5415f98d6
Daniel Gordon <daniel.gordon@ed.ac.uk>
2022-07-11 16:04:04 +0100

    Tidy up/finish plotting implementation

--- Scripts/1_CompareUpperLevelOptimisers/plot_results.py
+++ Scripts/1_CompareUpperLevelOptimisers/plot_results.py
@@ -2,36 +2,73 @@ import matplotlib.pyplot as plt
 import json
 import numpy as np
 
-save_file = "py_results100.json"
+METHODS = ['Bayesopt', 'RBF', 'CMA-ES']
+FUNCTIONS = ['SchafferF6', 'Sphere', 'Griewank', 'Rastrigin', 'Rosenbrock']
+MIN_YLIM = 1e-16
 
-with open(save_file, 'r') as f:
-    data = json.load(f)
+def plotBar(axes, data, title, ylims, ylabel, x_labels, y_labels, log=False):
+    n = len(x_labels)
+    m = len(y_labels)
+    for i in range(0, n):
+        x = np.linspace(i + 1, 2 * (m + 1) + i + m, m)
+        axes.bar(x, data[i, :], label = METHODS[i])
+        axes.title.set_text(title)
+        axes.set_ylabel(ylabel)
+        if log:
+            axes.set_yscale('log')
+        axes.set_ylim(ylims)
+        xtick_points = [(n + 1) / 2 + j * (n + 1) for j in range(1, m + 1)]
+        axes.set_xticks(xtick_points, FUNCTIONS)
+        axes.legend()
 
-n = len(data['times'][0])
-m = int(n/2)
-n_sets = 6
-colours = ['r', 'g', 'b']
-labels = ['Bayesopt', 'RBF', 'CMA-ES']
-fig = plt.figure()
-ax1 = fig.add_subplot(1, 2, 1)
-ax2 = fig.add_subplot(1, 2, 2)
-for i in range(0, 3):
-    x = np.linspace(i + 1, 2 * (m + 1) + i + m, m)
-    ax1.bar(x, data['times'][i][:m], label=labels[i])
-    ax2.bar(x, data['times'][i][m:], label=labels[i])
-    ax1.title.set_text('Deterministic')
-    ax2.title.set_text('Noisy')
-    ax1.set_ylabel('Time (s)')
-    ax2.set_ylabel('Time (s)')
-    ax1.set_yscale('log')
-    ax2.set_yscale('log')
-    y_max = 1.1*max(max(data['times']))
-    y_min = 0.9*min(min(data['times']))
-    ax1.set_ylim([y_min, y_max])
-    ax2.set_ylim([y_min, y_max])
-    ax1.set_xticks([2, 6, 10, 14, 18], ['SchafferF6', 'Sphere', 'Griewank', 'Rastrigin', 'Rosenbrock'])
-    ax2.set_xticks([2, 6, 10, 14, 18], ['SchafferF6', 'Sphere', 'Griewank', 'Rastrigin', 'Rosenbrock'])
+def getYLims(array):
+    min_lim, max_lim = MIN_YLIM, MIN_YLIM
+    if np.min(array) > 0:
+        min_lim = np.min(array)
+    if np.max(array) > 0:
+        max_lim = np.max(array)
+    ylims = [min_lim * (1 - 0.1 * np.sign(min_lim)), 
+        max_lim * (1 + 0.1 * np.sign(max_lim))] 
+    return ylims
+
+def customPlot(data, m, title, ylabel):
+
+    # Compute appropriate ylims
+    ylims = getYLims(data)
+
+    # Partition data
+    d_data = data[:, :m]
+    n_data = data[:, m:]
+
+    # Create overall figure
+    fig = plt.figure()
+    fig.suptitle(title)
+
+    # Add deterministic and noisy plots in turn
+    d_fig = fig.add_subplot(1, 2, 1)
+    plotBar(d_fig, d_data, 'Deterministic', ylims, ylabel, METHODS, FUNCTIONS, log=True)
+    n_fig = fig.add_subplot(1, 2, 2)
+    plotBar(n_fig, n_data, 'Noisy', ylims, ylabel, METHODS, FUNCTIONS, log=True)
+
+    # Add some overall figure text & show
     fig.text(0.5, 0.04, 'Optimisation Functions', ha='center', va='center', rotation='horizontal')
-    ax1.legend()
-    ax2.legend()
-plt.show()
+    plt.show()
+
+
+if __name__ == "__main__":
+    save_file = "py_results100.json"
+
+    with open(save_file, 'r') as f:
+        data = json.load(f)
+
+    # Organise data
+    time_data = np.array(data['times'])
+    value_data = np.array(data['values'])
+    value_data[0, :] = -value_data[0, :]
+    m = int(np.size(time_data, 1)/2)
+
+    # Plot time data
+    customPlot(time_data, m, 'Time', 'Time (s)')
+
+    # Plot value data
+    customPlot(value_data, m, 'Obtained Minima', 'Function Value')
