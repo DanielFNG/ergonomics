f7cd97d9c78499ac41a9c82f78111f18f12b7124
Daniel Gordon <daniel.gordon@ed.ac.uk>
2022-08-03 12:28:54 +0100

    Revert "Formatting + protection against rerunning normaliser/reference sims"
    This reverts commit 2ca0664b92ac2a7ed0d8ffb9b8a47a36ca6d7c12.

--- Scripts/15_PythonImplementation/python_ioc.py
+++ Scripts/15_PythonImplementation/python_ioc.py
@@ -2,16 +2,16 @@
 import os
 import subprocess
 import tempfile
-import json
 import numpy
 import PyNomad
+import json
 
 # High-level options
 N_EVALUATIONS = 1000
 REFERENCE_WEIGHTS = [0.1, 0.2, 0.3, 0.1, 0.2, 0.1]
 CONFIG_PATH = "config.txt"  # No need to give cluster_config here, even in cluster mode
 RESULTS_DIR = os.getcwd()
-_MODE = "cluster"
+_MODE = 'cluster'
 
 # Low-level options
 _UPPER_LIMIT = 1
@@ -19,10 +19,8 @@ _LOWER_LIMIT = 0
 _IDEAL_OPTIMISED_COST = 1
 _NORMALISER_FOLDER = "normalisers"
 _REFERENCE_FILE = "reference.sto"
-_EXECUTABLE_PRINT = os.path.join(os.getenv("ERGONOMICS_HOME"), "bin", "solveAndPrint")
-_EXECUTABLE_COMPARE = os.path.join(
-    os.getenv("ERGONOMICS_HOME"), "bin", "solveAndCompare"
-)
+_EXECUTABLE_PRINT = os.path.join(os.getenv('ERGONOMICS_HOME'), "bin", "solveAndPrint")
+_EXECUTABLE_COMPARE = os.path.join(os.getenv('ERGONOMICS_HOME'), "bin", "solveAndCompare")
 _OBJECTIVE_STR = "objective="
 _CLUSTER_WEIGHTS_FILE = "weights.txt"
 
@@ -93,7 +91,7 @@ def solve_constrained_nomad(func, dim, lb, ub, max_evals, n_seeds, mode):
         validity = []
 
         # Write weights file
-        with open(_CLUSTER_WEIGHTS_FILE, "w") as file:
+        with open(_CLUSTER_WEIGHTS_FILE, 'w') as file:
             for i in range(n_points):
                 x = block.get_x(i)
                 vals = [x.get_coord(i) for i in range(x.size())]
@@ -104,10 +102,10 @@ def solve_constrained_nomad(func, dim, lb, ub, max_evals, n_seeds, mode):
                     validity.append(True)
                     for val in vals:
                         file.write(str(val) + " ")
-                        file.write("\n")
+                        file.write('\n')
                 else:
                     validity.append(False)
-
+        
         # Dispatch to cluster
         command = ["qsub", "-sync", "y", "-t", "1-" + str(n_points), "cluster.sh"]
         subprocess.run(command, check=True)
@@ -118,7 +116,7 @@ def solve_constrained_nomad(func, dim, lb, ub, max_evals, n_seeds, mode):
             f = 0
             if validity[i]:
                 filepath = str(i) + ".sto"
-                with open(filepath, "r") as file:
+                with open(filepath, 'r') as file:
                     f = float(file.readline())
                 os.remove(filepath)
             rawBBO = str(f) + " " + gs[i]
@@ -130,6 +128,7 @@ def solve_constrained_nomad(func, dim, lb, ub, max_evals, n_seeds, mode):
 
         return eval_ok
 
+
     def local_objective(block):
         n_points = block.size()
         eval_ok = [False for i in range(n_points)]
@@ -139,26 +138,19 @@ def solve_constrained_nomad(func, dim, lb, ub, max_evals, n_seeds, mode):
             total = numpy.sum(numpy.array(vals))
             g = total - 1
             f = 0
-            if g <= 0:  # So we don't evaluate a useless point - too expensive
+            if g <= 0: # So we don't evaluate a useless point - too expensive
                 f = func(vals)
             rawBBO = str(f) + " " + str(g)
             x.setBBO(rawBBO.encode("UTF-8"))
             eval_ok[i] = True
         return eval_ok
 
-    params = [
-        "DIMENSION " + str(dim),
-        "BB_OUTPUT_TYPE OBJ EB",
-        "MAX_BB_EVAL " + str(max_evals),
-        "DIRECTION_TYPE ORTHO N+1 QUAD",
-        "DIRECTION_TYPE ORTHO 2N",
-        "DIRECTION_TYPE ORTHO N+1 NEG",
-        "DIRECTION_TYPE N+1 UNI",
-        "VNS_MADS_SEARCH yes",
-        "ANISOTROPIC_MESH no",
-        "BB_MAX_BLOCK_SIZE " + str(n_seeds),
-        "LH_SEARCH " + str(n_seeds) + " 0",
-    ]
+    params = ["DIMENSION " + str(dim), "BB_OUTPUT_TYPE OBJ EB", 
+        "MAX_BB_EVAL " + str(max_evals), 
+        "DIRECTION_TYPE ORTHO N+1 QUAD", "DIRECTION_TYPE ORTHO 2N", 
+        "DIRECTION_TYPE ORTHO N+1 NEG", "DIRECTION_TYPE N+1 UNI",
+        "VNS_MADS_SEARCH yes", "ANISOTROPIC_MESH no", 
+        "BB_MAX_BLOCK_SIZE " + str(n_seeds), "LH_SEARCH " + str(n_seeds) + " 0"]
 
     objective = local_objective
     if mode == "cluster":
@@ -171,7 +163,7 @@ def main():
     """Main script"""
     # Initial setup
     n_parameters = len(REFERENCE_WEIGHTS)
-    n_seeds = n_parameters ** 2
+    n_seeds = n_parameters**2
     normaliser_dir = os.path.join(RESULTS_DIR, _NORMALISER_FOLDER)
     reference_path = os.path.join(RESULTS_DIR, _REFERENCE_FILE)
     results_path = os.path.join(RESULTS_DIR, "results.json")
@@ -180,30 +172,22 @@ def main():
     simulate_normalisers(normaliser_dir, n_parameters)
 
     # Load normaliser results
-    if ~os.path.exists(normaliser_dir):
-        normalisers = compute_normalisers(
-            normaliser_dir, n_parameters, _IDEAL_OPTIMISED_COST
-        )
+    normalisers = compute_normalisers(
+        normaliser_dir, n_parameters, _IDEAL_OPTIMISED_COST
+    )
 
     # Compute reference
-    if ~os.path.exists(reference_path):
-        normalised_weights = numpy.divide(REFERENCE_WEIGHTS, normalisers)
-        run_lower_level_print(reference_path, normalised_weights)
+    normalised_weights = numpy.divide(REFERENCE_WEIGHTS, normalisers)
+    run_lower_level_print(reference_path, normalised_weights)
 
     # Use MADS to run upper-level optimisation
     inner_objective = lambda weights: objective(weights, normalisers, reference_path)
     result = solve_constrained_nomad(
-        inner_objective,
-        n_parameters,
-        _LOWER_LIMIT,
-        _UPPER_LIMIT,
-        N_EVALUATIONS,
-        n_seeds,
-        _MODE,
-    )
-
+        inner_objective, n_parameters, _LOWER_LIMIT, _UPPER_LIMIT, N_EVALUATIONS, 
+        n_seeds, _MODE)
+    
     # Save results to file
-    with open(results_path, "w") as f:
+    with open(results_path, 'w') as f:
         json.dump(result, f, indent=4)
 
 
